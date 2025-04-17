2e1e78efd5269c1bf829349a43f91ab460342b6e
gaidar.vu <gaidar.vu@gmail.com>
2025-04-17 23:18:05 +0300

    hw15.1

--- 15.1/main.tf
+++ 15.1/main.tf
@@ -2,6 +2,15 @@ resource "yandex_vpc_network" "my_network" {
   name = var.vpc_name
 }
 
+resource "yandex_vpc_route_table" "private_rt" {
+  network_id = yandex_vpc_network.my_network.id
+  name       = var.private_rt_name
+  static_route {
+    destination_prefix = var.private_dest_addr_pref
+    next_hop_address   = var.each_vm["vm3"].ip_address
+  }
+}
+
 resource "yandex_vpc_subnet" "public" {
   name           = var.public_cidr_name
   zone           = var.default_zone
@@ -12,6 +21,7 @@ resource "yandex_vpc_subnet" "private" {
   name           = var.private_cidr_name
   zone           = var.default_zone
   network_id     = yandex_vpc_network.my_network.id
+  route_table_id = yandex_vpc_route_table.private_rt.id
   v4_cidr_blocks = var.private_cidr
 }
 
@@ -31,49 +41,8 @@ data "template_file" "cloudinit" {
     vms_ssh_root_key = var.vms_ssh_root_key
   }
 }
-data "template_file" "nat_cloudinit" {
-  template = file("${path.module}/cloud-init/nat.yaml")
 
-  vars = {
-    vms_ssh_root_key = var.vms_ssh_root_key
-  }
-}
-
-resource "yandex_compute_instance" "nat-instance" {
-  name              = var.nat_vm.vm_name
-  hostname          = var.nat_vm.vm_name
-  platform_id       = var.nat_vm.platform_id
-  zone              = var.default_zone
-  resources {
-    cores           = var.nat_vm.cpu
-    memory          = var.nat_vm.ram
-    core_fraction   = var.nat_vm.core_fraction
-  }
-  boot_disk {
-    initialize_params {
-      image_id      = var.nat_vm.os_family
-      type          = var.nat_vm.type
-      size          = var.nat_vm.disk_volume
-    }
-  }
-  scheduling_policy {
-    preemptible     = var.nat_vm.scheduling_policy
-  }
-  network_interface {
-    subnet_id       = yandex_vpc_subnet.public.id
-    ip_address      = var.nat_vm.ip_address
-  }
-
-  metadata = merge(
-    var.metadata,
-    {
-      user-data          = data.template_file.nat_cloudinit.rendered
-      serial-port-enable = 1
-    }
-  )
-}
-
-resource "yandex_compute_instance" "public-instance" {
+resource "yandex_compute_instance" "my_network_instances" {
   for_each = var.each_vm
   name              = each.value.vm_name
   hostname          = each.value.vm_name
@@ -97,6 +66,7 @@ resource "yandex_compute_instance" "public-instance" {
   network_interface {
     subnet_id = local.subnet_map[each.value.subnet_name]
     nat             = each.value.network_interface
+    ip_address      = each.value.ip_address
   }
 
   metadata = merge(
--- 15.1/variables.tf
+++ 15.1/variables.tf
@@ -38,6 +38,16 @@ variable "private_cidr_name" {
   default     = "private"
   description = "subnet name"
 }
+variable "private_rt_name" {
+  type        = string
+  default     = "private-route-table"
+  description = "route-table name"
+}
+variable "private_dest_addr_pref" {
+  type        = string
+  default     = "0.0.0.0/0"
+  description = "route-table name"
+}
 variable "vms_ssh_root_key" {
   type        = string
   description = "ssh-keygen -t ed25519"
@@ -55,6 +65,7 @@ variable "each_vm" {
     scheduling_policy=bool
     os_family=string
     subnet_name=string
+    ip_address=string
     }))
   default = {
     "vm1" = {
@@ -69,6 +80,7 @@ variable "each_vm" {
       scheduling_policy=true
       os_family="fd8jnll1ou4fv2gil3rv"
       subnet_name="public"
+      ip_address="192.168.10.10"
     }
     "vm2" = {
       platform_id="standard-v3"
@@ -82,35 +94,21 @@ variable "each_vm" {
       scheduling_policy=true
       os_family="fd8jnll1ou4fv2gil3rv"
       subnet_name="private"
+      ip_address="192.168.20.20"
     }
-  }
-}
-
-variable "nat_vm" {
-  description = "Configuration for NAT instance"
-  type = object({
-    platform_id       = string
-    vm_name           = string
-    cpu               = number
-    ram               = number
-    core_fraction     = number
-    type              = string
-    disk_volume       = number
-    scheduling_policy = bool
-    os_family         = string
-    ip_address        = string
-  })
-
-  default = {
-    platform_id       = "standard-v3"
-    vm_name           = "nat-instance"
-    cpu               = 2
-    ram               = 1
-    core_fraction     = 20
-    type              = "network-hdd"
-    disk_volume       = 10
-    scheduling_policy = true
-    os_family         = "fd80mrhj8fl2oe87o4e1"
-    ip_address        = "192.168.10.254"
+    "vm3" = {
+      platform_id="standard-v3"
+      vm_name="nat-instance"
+      cpu=2
+      ram=1
+      core_fraction=20
+      type="network-hdd"
+      disk_volume=10
+      network_interface=true
+      scheduling_policy=true
+      os_family="fd80mrhj8fl2oe87o4e1"
+      subnet_name="public"
+      ip_address="192.168.10.254"
+    }
   }
 }
